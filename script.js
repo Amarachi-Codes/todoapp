ca8899e9c3291b552d44747ef8980f209da55dcb
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-09 22:33:49 +0100

    updated

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 
-document.addEventListener("DOMContentLoaded",()=>{
-const taskInput = document.getElementById("task");
-const enterbtn = document.getElementById("enter");
-const list = document.getElementById("list");
-const taskInputValue = taskInput.value.trim()
+// document.addEventListener("DOMContentLoaded",()=>{
+// const taskInput = document.getElementById("task");
+// const enterbtn = document.getElementById("enter");
+// const list = document.getElementById("list");
+// const taskInputValue = taskInput.value.trim()
 
-enterbtn.addEventListener("click",()=>{
-    if(taskInputValue !== " "){
-        const li = document.createElement("li")
-        li.textContent = taskInputValue
-        list.appendChild(li);
+// enterbtn.addEventListener("click",()=>{
+//     if(taskInputValue !== " "){
+//         const li = document.createElement("li")
+//         li.textContent = taskInputValue
+//         list.appendChild(li);
         
-        //clear input value
-        taskInput.value = " "
+//         //clear input value
+//         taskInput.value = " "
         
-    }
-})
+//     }
+// })
 
 
+// })
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const taskInput = document.querySelector("#taskInput");
+    const button =document.querySelector('#button')
+    const tasklist = document.querySelector('#tasklist')
 })
